e5b9d4128b6ed5b8eddab2460f761129cd91b4bd
Hanamant <channal.hanamant@gmail.com>
2026-05-24 10:24:22 +0530

    Created Selenium WebDriver Module

---
+++ src/main/java/com/webdriver/intro/Demo.java
@@ -0,0 +1,14 @@
+package com.webdriver.intro;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		WebDriver wd=new ChromeDriver();
+		wd.get("https://www.google.com");
+
+	}
+
+}
