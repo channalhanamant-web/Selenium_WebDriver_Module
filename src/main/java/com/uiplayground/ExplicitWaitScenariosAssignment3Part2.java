9829c097e111fbbc5b6ff0368a5f0a31655cd7fd
Hanamant <channal.hanamant@gmail.com>
2026-06-01 00:37:17 +0530

    Task 5 and 6

--- src/main/java/com/uiplayground/ExplicitWaitScenariosAssignment3Part2.java
+++ src/main/java/com/uiplayground/ExplicitWaitScenariosAssignment3Part2.java
@@ -1,6 +1,8 @@
 package com.uiplayground;
 
 import java.time.Duration;
+
+import org.jspecify.annotations.NonNull;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -23,24 +25,82 @@ public class ExplicitWaitScenariosAssignment3Part2 {
 		explicitWaitTextBox.click();
 
 		System.out.println("........Task 4 ..........");
-		
-		//div[@data-testid="wait-invisible-target"]
+	
 		
 		By loadTextDisplayLocator=By.xpath("//div[@data-testid=\"wait-invisible-target\"]");
 		WebElement loadTextDisplay=wait.until(ExpectedConditions.visibilityOfElementLocated(loadTextDisplayLocator));
 		boolean loadText= loadTextDisplay.isDisplayed();
 		System.out.println(loadText);
 
-		//button[@data-testid="btn-trigger-invisible"]
 		
 		By triggerButtonLocator=By.xpath("//button[@data-testid=\"btn-trigger-invisible\"]");
 		WebElement triggerButton=wait.until(ExpectedConditions.elementToBeClickable(triggerButtonLocator));
 		triggerButton.click();
 		
-		//By loadTextDisplayLocator=By.xpath("//div[@data-testid=\"wait-invisible-target\"]");
-		//loadTextDisplay=wait.until(ExpectedConditions.invisibilityOfElementLocated(loadTextDisplayLocator));
+	
+		Boolean invisibleSpinner =wait.until(ExpectedConditions.invisibilityOfElementLocated(loadTextDisplayLocator));
+		System.out.println(invisibleSpinner);
+		
+		
+		
+		System.out.println("........Task 5 ..........");
+		
+		
+		By divTargetLocator=By.xpath("//div[@data-testid='wait-stale-target']");
+		WebElement divTarget=wait.until(ExpectedConditions.visibilityOfElementLocated(divTargetLocator));
+		System.out.println(divTarget.getAttribute("data-render-key"));
+		
+		
 
-
+	    By divTargetTriggerButtonLocator=By.xpath("//button[@data-testid=\"btn-trigger-stale\"]");
+		WebElement divTargetTriggerButton=wait.until(ExpectedConditions.elementToBeClickable(divTargetTriggerButtonLocator));
+		divTargetTriggerButton.click();
+		
+		System.out.println(divTarget.getAttribute("data-render-key"));
+		
+		boolean status =wait.until(ExpectedConditions.stalenessOf(divTarget));
+		System.out.println(status);
+		WebElement divTargetNew=wait.until(ExpectedConditions.visibilityOfElementLocated(divTargetLocator));
+		System.out.println(divTargetNew.getAttribute("data-render-key"));
+		
+		
+		
+		System.out.println("........Task 5 ..........");
+		
+		//input[@data-testid="wait-selected-target"]
+		
+	    By checkBoxLocator=By.xpath("//input[@data-testid=\"wait-selected-target\"]");
+	    WebElement checkBox=wait.until(ExpectedConditions.elementToBeClickable(checkBoxLocator));
+		boolean checkBoxSelect=checkBox.isSelected();
+		System.out.println(checkBoxSelect);
+		
+		By checkBoxTriggerButtonLocator=By.xpath("//button[@data-testid=\"btn-trigger-selected\"]");
+		WebElement checkBoxTriggerButton=wait.until(ExpectedConditions.elementToBeClickable(checkBoxTriggerButtonLocator));
+		checkBoxTriggerButton.click();
+		
+		Boolean checkBoxNew=wait.until(ExpectedConditions.elementToBeSelected(checkBoxLocator));
+		System.out.println(checkBoxNew);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
