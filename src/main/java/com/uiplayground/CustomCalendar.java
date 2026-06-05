6c5b86c6c599ca1c176eff89b031d3e3676a74dd
Hanamant <channal.hanamant@gmail.com>
2026-06-05 12:42:43 +0530

    Edited class name

--- src/main/java/com/uiplayground/CustomCalendar.java
+++ src/main/java/com/uiplayground/CustomCalendar.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class CustomerCalendar {
+public class CustomCalendar {
 
 	public static void main(String[] args) {
 
